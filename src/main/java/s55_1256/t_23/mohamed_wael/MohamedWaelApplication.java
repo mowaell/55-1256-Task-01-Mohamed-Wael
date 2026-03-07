ea595e0c5ca8adbc5086f19bfd7203e7e3c41380
mowaell <mwael0304@gmail.com>
2026-03-07 23:05:22 +0200

    Initial commit: project setup

---
+++ src/main/java/s55_1256/t_23/mohamed_wael/MohamedWaelApplication.java
@@ -0,0 +1,13 @@
+package s55_1256.t_23.mohamed_wael;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MohamedWaelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MohamedWaelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/s55_1256/t_23/mohamed_wael/MohamedWaelApplicationTests.java
@@ -0,0 +1,13 @@
+package s55_1256.t_23.mohamed_wael;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MohamedWaelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
